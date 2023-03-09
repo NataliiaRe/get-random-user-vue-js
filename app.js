94a28f9d163683dda32d292b0618995fbf40aa24
NataliiaRe <bortnyknatasha@gmail.com>
2023-03-09 18:15:19 +0200

    Added data and created an HTML structure to the user page

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const app = Vue.createApp({
-  template: "<h1>Hello World</h1>",
+  data() {
+    return {
+      firstName: "Maxym",
+      lastName: "Rekunenko",
+      email: "max@gmail.com",
+      gender: "male",
+      picture:
+        "https://www.clipartmax.com/png/small/4-46714_spiderman-clipart-deviantart-spider-man-baby-png.png",
+    };
+  },
 });
 
 app.mount("#app");
